8f7590008e2ff7ddafe348e151a512be2a151183
João Pedro Ayruth Lucatto <69975970+JoaoPedroLucatto@users.noreply.github.com>
2023-07-22 10:48:46 -0300

    first commit

---
+++ js/custom.js
@@ -0,0 +1,38 @@
+
+//
+$(document).ready(function() {
+	$('#fullpage').fullpage({
+		'verticalCentered': false,
+		'scrollingSpeed': 800,
+		'autoScrolling': false,
+		'css3': true,
+		'navigation': true,
+		'navigationPosition': 'right',
+		'setResponsive': true,
+	});
+
+	setTimeout(function() {
+		var typed = new Typed('#element', {
+		  strings: [
+			"Hi! I'm João Pedro"
+		  ],
+		  typeSpeed: 100,
+		  contentType: 'html'
+		});
+		var typed2 = new Typed('#element2', {
+			strings: [
+			  "Software Developer | Ruby on Rails | Go | API | DevOps"
+			],
+			typeSpeed: 180,
+			contentType: 'html'
+		  });
+	}, 500);
+
+});
+
+// wow
+$(function()
+{
+    new WOW().init();
+    $(".rotate").textrotator();
+})
